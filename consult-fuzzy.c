e8f07f238f436a36916afa65bac5df7bc42b209f
Daniel Mendler <mail@daniel-mendler.de>
2021-01-28 04:08:12 +0100

    work on sync

--- consult-fuzzy.c
+++ consult-fuzzy.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
@@ -14,7 +15,7 @@ static void chk(const char* str, int err) {
     }
 }
 
-static void wait_proc(pid_t* pid, int* fd) {
+static void source_end(pid_t* pid, int* fd) {
     close(*fd);
     chk("waitpid failed", waitpid(*pid, 0, 0) < 0);
     *pid = *fd = -1;
@@ -32,37 +33,45 @@ static void source_start(pid_t* pid, int* fd, const char* cmd) {
     }
     close(p[1]);
     chk("fcntl failed", fcntl(p[0], F_SETFL, O_NONBLOCK) < 0);
+    chk("fcntl failed", fcntl(p[0], F_SETPIPE_SZ, 0x100000) < 0);
     *fd = p[0];
 }
 
-static void filter_end(pid_t* pid, int* fd) {
-    wait_proc(pid, fd);
+static void filter_end(pid_t* pid, int* wfd, int* rfd) {
+    close(*wfd);
+    close(*rfd);
+    chk("waitpid failed", waitpid(*pid, 0, 0) < 0);
+    *pid = *wfd = *rfd = -1;
     chk("write failed", write(STDOUT_FILENO, "\3", 1) != 1); // ETX
 }
 
-static void filter_start(pid_t* pid, int* fd, const char* cmd, const char* filter) {
+static void filter_start(pid_t* pid, int* wfd, int* rfd, const char* cmd, const char* filter) {
     if (*pid >= 0) {
         chk("kill failed", kill(*pid, SIGKILL) < 0);
-        filter_end(pid, fd);
+        filter_end(pid, wfd, rfd);
     }
-    int p[2];
-    chk("pipe failed", pipe(p) < 0);
+    int p[2], q[2];
+    chk("pipe failed", pipe(p) < 0 || pipe(q) < 0);
     chk("fork failed", (*pid = fork()) < 0);
     if (!*pid) {
-        chk("dup2 failed", dup2(p[0], STDIN_FILENO));
+        chk("dup2 failed", dup2(p[0], STDIN_FILENO) < 0 || dup2(q[1], STDOUT_FILENO) < 0);
         close(p[1]);
+        close(q[0]);
         execl("/bin/sh", "sh", "-c", cmd, filter, 0);
         chk("execl failed", 1);
     }
     close(p[0]);
-    chk("fcntl failed", fcntl(p[1], F_SETFL, O_NONBLOCK) < 0);
+    close(q[1]);
+    chk("fcntl failed", fcntl(p[1], F_SETFL, O_NONBLOCK) < 0 || fcntl(q[0], F_SETFL, O_NONBLOCK) < 0);
+    chk("fcntl failed", fcntl(p[1], F_SETPIPE_SZ, 0x100000) < 0 || fcntl(q[0], F_SETPIPE_SZ, 0x100000) < 0);
     chk("write failed", write(STDOUT_FILENO, "\2", 1) != 1); // STX
-    *fd = p[1];
+    *wfd = p[1];
+    *rfd = q[0];
 }
 
 int main(int argc, char* argv[]) {
     pid_t filter_pid = -1, source_pid = -1;
-    int filter_fd = -1, source_fd = -1;
+    int filter_wfd = -1, filter_rfd = -1, source_fd = -1;
     char* data = 0;
     size_t data_size = 0, data_written = 0, data_avail = 0;
 
@@ -89,19 +98,24 @@ int main(int argc, char* argv[]) {
                 max_fd = source_fd;
             FD_SET(source_fd, &read_fds);
         }
+        if (filter_rfd >= 0) {
+            if (filter_rfd > max_fd)
+                max_fd = filter_rfd;
+            FD_SET(filter_rfd, &read_fds);
+        } // TODO helper
 
         FD_ZERO(&write_fds);
-        if (filter_fd >= 0 && data_written < data_size) {
-            if (filter_fd > max_fd)
-                max_fd = filter_fd;
-            FD_SET(filter_fd, &write_fds);
+        if (filter_wfd >= 0 && data_written < data_size) {
+            if (filter_wfd > max_fd)
+                max_fd = filter_wfd;
+            FD_SET(filter_wfd, &write_fds);
         }
 
         chk("select failed", select(max_fd + 1, &read_fds, &write_fds, 0, 0) < 0);
 
-        // Read new filter string
+        // Read new filter string from stdin
         if (FD_ISSET(STDIN_FILENO, &read_fds)) {
-            char buf[1024];
+            char buf[0x400];
             ssize_t len = read(STDIN_FILENO, buf, sizeof (buf));
             chk("read from stdin failed", len < 0);
             if (!len)
@@ -112,9 +126,21 @@ int main(int argc, char* argv[]) {
             }
             if (len > 1) {
                 buf[len - 1] = 0;
-                filter_start(&filter_pid, &filter_fd, argv[2], buf);
+                filter_start(&filter_pid, &filter_wfd, &filter_rfd, argv[2], buf);
                 data_written = 0;
             }
+        }
+
+        // Read filtered data
+        if (filter_rfd >= 0 && FD_ISSET(filter_rfd, &read_fds)) {
+            char buf[0x100000];
+            ssize_t len = read(filter_rfd, buf, sizeof (buf));
+            if (len < 0)
+                chk("read from filter failed", errno != EAGAIN);
+            else if (len)
+                chk("write to stdout failed", write(STDOUT_FILENO, buf, len) < 0); // TODO all
+            else
+                filter_end(&filter_pid, &filter_wfd, &filter_rfd);
         }
 
         // Read new data from source
@@ -125,19 +151,19 @@ int main(int argc, char* argv[]) {
             }
             ssize_t len = read(source_fd, data + data_size, data_avail - data_size);
             if (len < 0)
-                chk("read from stdin failed", errno != EAGAIN);
+                chk("read from source failed", errno != EAGAIN);
             else if (len)
                 data_size += len;
             else
-                wait_proc(&source_pid, &source_fd);
+                source_end(&source_pid, &source_fd);
         }
 
         // Write new data to filter
-        if (filter_fd >= 0 && FD_ISSET(filter_fd, &write_fds) && data_written < data_size) {
-            ssize_t len = write(filter_fd, data + data_written, data_size - data_written);
+        if (filter_wfd >= 0 && FD_ISSET(filter_wfd, &write_fds) && data_written < data_size) {
+            ssize_t len = write(filter_wfd, data + data_written, data_size - data_written);
             if (len < 0) {
                 if (errno == EPIPE)
-                    filter_end(&filter_pid, &filter_fd);
+                    filter_end(&filter_pid, &filter_wfd, &filter_rfd);
                 else
                     chk("write failed", errno != EAGAIN);
             } else {
@@ -145,9 +171,11 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        // Source is done, wait for filter
-        if (source_fd < 0 && filter_fd >= 0 && data_written == data_size)
-            filter_end(&filter_pid, &filter_fd);
+        // Source is done, all data has been written, close filter
+        if (source_fd < 0 && filter_wfd >= 0 && data_written == data_size) {
+            close(filter_wfd);
+            filter_wfd = -1;
+        }
     }
 
     return 0;
